9bf8488c7770b63c9b475a9cf3338bbf35017341
jessen reinhart <jessen_1206@yahoo.com>
2026-08-29 00:28:57 +0700

    Widen authored building shadow silhouettes

--- game/systems/BuildingSpriteVisuals.ts
+++ game/systems/BuildingSpriteVisuals.ts
@@ -23,50 +23,50 @@ export type BuildingSpriteVisualConfig = {
 export const BUILDING_SPRITE_VISUALS: Record<BuildingType, BuildingSpriteVisualConfig> = {
     [BuildingType.TOWN_CENTER]: {
         key: 'townhall', scaleMultiplier: 1.2, originY: 0.75,
-        shadowHeightScale: 1.0, shadowFootprintScale: 0.90, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.58,
+        shadowHeightScale: 1.0, shadowFootprintScale: 1.12, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.90,
     },
     [BuildingType.HOUSE]: {
         key: 'house', scaleMultiplier: 1.6, originY: 0.85,
-        shadowHeightScale: 0.72, shadowFootprintScale: 0.96, shadowAnchorOffsetY: 6, shadowEndWidthScale: 0.56,
+        shadowHeightScale: 0.72, shadowFootprintScale: 1.18, shadowAnchorOffsetY: 6, shadowEndWidthScale: 0.92,
     },
     [BuildingType.BARRACKS]: {
         key: 'barracks', scaleMultiplier: 1.5, originY: 0.75,
-        shadowHeightScale: 0.74, shadowFootprintScale: 0.96, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.58,
+        shadowHeightScale: 0.74, shadowFootprintScale: 1.12, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.90,
     },
     [BuildingType.FARM]: {
         key: 'field', scaleMultiplier: 1.3, originY: 0.5,
-        shadowHeightScale: 0.24, shadowFootprintScale: 0.94, shadowAnchorOffsetY: 2, shadowEndWidthScale: 0.76,
+        shadowHeightScale: 0.24, shadowFootprintScale: 1.06, shadowAnchorOffsetY: 2, shadowEndWidthScale: 0.96,
     },
     [BuildingType.LUMBER_CAMP]: {
         key: 'lumber', scaleMultiplier: 1.7, originY: 0.75,
-        shadowHeightScale: 0.72, shadowFootprintScale: 0.96, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.60,
+        shadowHeightScale: 0.72, shadowFootprintScale: 1.14, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.92,
     },
     [BuildingType.HUNTERS_LODGE]: {
         key: 'lodge', scaleMultiplier: 1.6, originY: 0.75,
-        shadowHeightScale: 0.68, shadowFootprintScale: 0.94, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.60,
+        shadowHeightScale: 0.68, shadowFootprintScale: 1.10, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.92,
     },
     [BuildingType.BONFIRE]: {
         key: 'bonfire', scaleMultiplier: 2.1, originY: 0.82,
-        shadowHeightScale: 0.18, shadowFootprintScale: 0.70, shadowAnchorOffsetY: 3, shadowEndWidthScale: 0.72,
+        shadowHeightScale: 0.18, shadowFootprintScale: 0.76, shadowAnchorOffsetY: 3, shadowEndWidthScale: 0.88,
     },
     [BuildingType.SMALL_PARK]: {
         key: 'park', scaleMultiplier: 1.5, originY: 0.82,
-        shadowHeightScale: 0.24, shadowFootprintScale: 0.88, shadowAnchorOffsetY: 3, shadowEndWidthScale: 0.72,
+        shadowHeightScale: 0.24, shadowFootprintScale: 1.00, shadowAnchorOffsetY: 3, shadowEndWidthScale: 0.95,
     },
     [BuildingType.MARKET]: {
         key: 'market', scaleMultiplier: 1.8, originY: 0.78,
-        shadowHeightScale: 0.72, shadowFootprintScale: 0.96, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.60,
+        shadowHeightScale: 0.72, shadowFootprintScale: 1.14, shadowAnchorOffsetY: 5, shadowEndWidthScale: 0.92,
     },
     [BuildingType.WALL]: {
         key: 'wall', scaleMultiplier: 2.4, originY: 0.76,
-        shadowHeightScale: 0.62, shadowFootprintScale: 0.90, shadowAnchorOffsetY: 4, shadowEndWidthScale: 0.68,
+        shadowHeightScale: 0.62, shadowFootprintScale: 1.04, shadowAnchorOffsetY: 4, shadowEndWidthScale: 0.94,
     },
     [BuildingType.CATHEDRAL]: {
         key: 'cathedral', scaleMultiplier: 1.7, originY: 0.82,
-        shadowHeightScale: 1.0, shadowFootprintScale: 0.96, shadowAnchorOffsetY: 7, shadowEndWidthScale: 0.54,
+        shadowHeightScale: 1.0, shadowFootprintScale: 1.14, shadowAnchorOffsetY: 7, shadowEndWidthScale: 0.88,
     },
     [BuildingType.CASTLE]: {
         key: 'castle', scaleMultiplier: 1.5, originY: 0.80,
-        shadowHeightScale: 1.0, shadowFootprintScale: 0.98, shadowAnchorOffsetY: 8, shadowEndWidthScale: 0.54,
+        shadowHeightScale: 1.0, shadowFootprintScale: 1.16, shadowAnchorOffsetY: 8, shadowEndWidthScale: 0.88,
     },
 };
